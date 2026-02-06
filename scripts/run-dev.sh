6f6817e6c93993842920510bf2862a37d4f863b0
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2026-02-06 21:20:20 +0000

    feat: add run script

---
+++ scripts/run-dev.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ "$(basename "$PWD")" == "scripts" ]; then
+  cd ..
+fi
+
+function start_ollama() {
+    if ! pgrep -x "ollama" > /dev/null; then
+    echo "Starting Ollama server..."
+    tmux new-session -d -s Ollama "ollama serve"
+    # wait for ollama to start
+    sleep 5
+    # curl ollama to check until it's running
+    until curl -s http://localhost:11434/v1/models > /dev/null; do
+      echo "Waiting for Ollama server to start..."
+      sleep 2
+    done
+  else
+    echo "Ollama server is already running."
+  fi
+}
+
+start_ollama
+
+# switch to java 21
+JAVA_HOME="$(/usr/libexec/java_home -v 21)"
+export JAVA_HOME
+
+docker compose up -d
+./gradlew quarkusDev
